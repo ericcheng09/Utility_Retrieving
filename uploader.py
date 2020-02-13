5bbf5e66bcb42c69e5e249d9f81dfa2f7cb64c23
Fu-Yuan Cheng <fxc3771@g.rit.edu>
2020-02-13 11:37:23 +0800

    Add configparse for configuration. Implement class for retrieving docker utilities.

--- uploader.py
+++ uploader.py
@@ -1,26 +1,46 @@
 from influxdb import InfluxDBClient
-import argparse
+import argparse, configparser
 import signal
 import time
+import hostutil, dockerutil, pmemutil
+
 
 def signal_handler(signal, frame):
     global interrupted
     interrupted = True
 
+
 signal.signal(signal.SIGINT, signal_handler)
-
-
 interrupted = False
+
 parser = argparse.ArgumentParser()
-parser.add_argument("--IP", help="IP of database", dest="ip", default="127.0.0.1")
-parser.add_argument("--port", help="Port of database", dest="port", default=8086)
-parser.add_argument("--db", help="Name of database", dest="db", default="PM_Demo")
-parser.add_argument("--user", help="User of database", dest="user", default="root")
-parser.add_argument("--pass", help="Password of User", dest="password", default="root")
-
-
+parser.add_argument("--config", help="path of config file", dest="config_path", default="./config.ini")
 args = parser.parse_args()
-client = InfluxDBClient(host=args.ip, port=args.port, username=args.user, password=args.password, database=args.db)
+config_path = args.config_path
+
+config = configparser.ConfigParser()
+config.read(config_path)
+
+client = InfluxDBClient(host=config["BASIC"]["ip"],
+                        port=int(config["BASIC"]["port"]),
+                        username=config["BASIC"]["user"],
+                        password=config["BASIC"]["password"],
+                        database=config["BASIC"]["database"])
+
+source = []
+
+if bool(config["BASIC"]["Host"]):
+    source.append(hostutil.HostUtil())
+
+if bool(config["BASIC"]["Docker"]):
+    source.append(dockerutil.DockerUtil(config["DOCKER"]["base_url"],
+                                        config["DOCKER"]["include_all"],
+                                        [container for container in config.items("DOCKER.CONTAINERS")])
+                  )
+
+if bool(config["BASIC"]["PMEM"]):
+    # source.append(pmemutil.PMEM())
+    pass
 
 while True:
     data = []
@@ -28,8 +48,8 @@ while True:
         print("Closing...")
         break
     time.sleep(1)
+    for s in source:
+        source.extend(s.get_data())
 
     client.write_points(data)
-
-
 client.close()
