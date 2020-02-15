55da1b90428d9e6bef73bb639ef7a968aabcfa0b
Fu-Yuan Cheng <fxc3771@g.rit.edu>
2020-02-15 10:56:16 +0800

    Add new fields for PMEM measurement

--- pmemutil.py
+++ pmemutil.py
@@ -6,11 +6,62 @@ class PMEM():
 
     def __init__(self):
         self.host = os.uname()[1]
-        self.command = "ipmctl show -performance MediaReads,MediaWrites"
+        # self.health_stats_mapping = {
+        #     "Healthy": 0,
+        #     "Noncritical": 1,
+        #     "Critical": 2,
+        #     "Fatal": 3,
+        #     "Non-Functional": 4,
+        #     "Unmanageable": 5,
+        #     "Unknown": 6
+        # }
+        # self.device_info = {}
+        # self.devices = []
+        # self._get_all_pmem()
+        self.sensor_info = {}
+        self._get_sensor_info()
+
+
+    # def _get_all_pmem(self): # discover devices
+    #     output = str(subprocess.check_output("ipmctl show -a -u B -dimm", shell=True))
+    #     keys = re.findall("[a-zA-Z]+=", output)
+    #     keys = [key[:len(key) - 1] for key in keys]
+    #     # values = re.findall("=[a-zA-Z0-9]+", output)
+    #     values = re.findall("=.+\n", output)
+    #     values = [value[1:len(value) - 1] for value in values]
+    #     for idx, key in enumerate(keys):
+    #         if key == "DimmID":
+    #             id = values[idx].replace("-","")
+    #             self.devices.append(id)
+    #             self.device_info[id] = {}
+    #             continue
+    #         self.device_info[self.devices[-1]][key] = values[idx]
+
+    def _get_sensor_info(self):
+        output = str(subprocess.check_output("ipmctl show -a -sensor -dimm", shell=True))
+        keys = re.findall("[a-zA-Z]+=", output)
+        keys = [key[:- 1] for key in keys]
+        # values = re.findall("=[a-zA-Z0-9]+", output)
+        values = re.findall("=.+\n", output)
+        values = [value[1:- 1] for value in values]
+
+        global tmp_id, tmp_type
+        for idx, key in enumerate(keys):
+            if key == "DimmID":
+                dimmid = values[idx].replace("-","")
+                tmp_id = dimmid
+                self.sensor_info[dimmid] = {}
+                continue
+            elif key == "Type":
+                tmp_type = values[idx]
+                self.sensor_info[tmp_id][tmp_type] = {}
+                continue
+            self.sensor_info[tmp_id][tmp_type][key] = values[idx]
+
 
     def get_data(self):
         data = []
-        output = str(subprocess.check_output(self.command, shell=True))
+        output = str(subprocess.check_output("ipmctl show -performance MediaReads,MediaWrites", shell=True))
         data_dict = {}
         keys = re.findall("[a-zA-Z]+=", output)
         values = re.findall("=[a-zA-Z0-9]+", output)
@@ -31,8 +82,11 @@ class PMEM():
                     },
                     "fields": {
                         "MediaReads": int(data_dict["MediaReads"][idx], 0) * 64,
-                        "MediaWrites": int(data_dict["MediaWrites"][idx], 0) * 64
+                        "MediaWrites": int(data_dict["MediaWrites"][idx], 0) * 64,
+                        "Health": self.sensor_info[Dimm]["Health"]["CurrentValue"]
                     }
                 }
             )
+
+
         return data
